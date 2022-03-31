f0a010519e0cf6bf063796eb954487aa1c93f644
Vince Andriacco <75548898+vandriacco@users.noreply.github.com>
2022-03-31 11:40:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+import os
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if (message.author == client.user):
+        return
+
+
+client.run(os.getenv('TOKEN'))
