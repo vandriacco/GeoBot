9a31fd9f7c521f61e7bb3b6a2a09ebbfc425c806
Vince Andriacco <75548898+vandriacco@users.noreply.github.com>
2022-03-31 11:52:20 +0000

    Add a ping pong function, where and message including '!!ping!' will prompt a response of 'pong!' from the bot.

--- main.py
+++ main.py
@@ -14,5 +14,7 @@ async def on_message(message):
     if (message.author == client.user):
         return
 
+    if (message.content.startswith('!!ping!')):
+      await message.channel.send('pong!')
 
 client.run(os.getenv('TOKEN'))
