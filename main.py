82029836d6d504f4aaf176ecdc487fc4eadc9ad2
Vince Andriacco <75548898+vandriacco@users.noreply.github.com>
2022-04-06 23:04:06 +0000

    I added a feature to convert currencies using the exchangerate api.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 import os
+import requests
+import re
 
 client = discord.Client()
 
@@ -16,5 +18,22 @@ async def on_message(message):
 
     if (message.content.startswith('!!ping!')):
       await message.channel.send('pong!')
+      
+    convert_str = '-convert'
+    if (message.content.startswith(convert_str)):
+      command = message.content[len(convert_str)+1:]
+      tokens = command.split(' ')
+      
+      parameters = {
+        "from": tokens[0],
+        "to": tokens[2],
+        "amount": float(tokens[4])
+      }
+
+      url = 'https://api.exchangerate.host/convert?from={}&to={}'
+      response = requests.get(url.format(parameters["from"], parameters["to"]))
+      data = response.json()
+      await message.channel.send(data['result'] * parameters["amount"])
+
 
 client.run(os.getenv('TOKEN'))
